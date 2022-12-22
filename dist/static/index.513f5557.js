d257ff77c6eed981e98d74d8ad089dce17298e85
lukelili <lipeng5231@163.com>
2022-12-22 17:37:32 +0800

    '删掉dist'

--- dist/static/index.513f5557.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index.6b1f6cda.js";import"./vendor.a9c2ed88.js";const e={};function t(_,c){return"\u767B\u5F55\u9875\u9762"}var a=r(e,[["render",t]]);export{a as default};
--- dist/static/index.eccef313.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.6b1f6cda.js";import{d as _,h as o,o as t}from"./vendor.a9c2ed88.js";const s={class:"home"},a=_({setup(r){return(c,n)=>(t(),o("div",s))}});var i=e(a,[["__scopeId","data-v-d77bce00"]]);export{i as default};
